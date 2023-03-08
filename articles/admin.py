1eee4042065b0567bc08b0a3033b710ea6c09c22
Alexander Bogdashkin <a_bogdashkin@mail.ru>
2023-03-08 13:18:04 +0300

    add homework

---
+++ articles/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
+
+from .models import Article, Tag, Scope
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        test = True
+        count = 0
+        for form in self.forms:
+            if 'is_main' in form.cleaned_data:
+                if test == form.cleaned_data.get('is_main'):
+                    count += 1
+        if count == 0:
+            raise ValidationError('Укажите основной раздел')
+        if count > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        return super().clean()
+        
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline, ]
+    
+
+# @admin.register(Tag)
+# class TagAdmin(admin.ModelAdmin):
+#     inlines = [ScopeInline, ]
